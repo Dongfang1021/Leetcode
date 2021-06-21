fc5ef0a8783e03907f2e4c69ac5335877a25da7b
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-06-21 05:31:39 -0700

    Time: 40 ms (75.49%), Space: 14.5 MB (92.52%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,15 +5,15 @@ class Solution:
                 return 0
             while left <= right:
                 pivot = (left + right) // 2
-                if nums[pivot] > nums[pivot + 1]:
+                if nums[pivot] > nums[pivot + 1]: # nums[pivot] is the largest number in nums
                     return pivot + 1
                 else:
-                    if nums[pivot] < nums[left]:
+                    if nums[pivot] < nums[left]: # nums[pivot] after rotate index
                         right = pivot - 1
-                    else:
+                    else: # nums[pivot] before rotate index
                         left = pivot + 1
-                        
-        def search(left, right):
+        
+        def search(left, right): # binary search function
             while left <= right:
                 pivot = (left + right) // 2
                 if nums[pivot] == target:
@@ -26,7 +26,7 @@ class Solution:
             return -1
         
         n = len(nums)
-        if n == 1:
+        if n == 1: # array just has one integer
             return 0 if nums[0] == target else -1
         rotate_index = find_rotate_index(0, n-1)
         if nums[rotate_index] == target:
@@ -36,4 +36,5 @@ class Solution:
         if target < nums[0]:
             return search(rotate_index, n - 1)
         return search(0, rotate_index)
+    
         
