7715d23ec0fa3eb5185a8a836f9086011cbefc3a
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-07-12 16:40:11 -0700

    Time: 692 ms (91.09%), Space: 17.5 MB (50.84%) - LeetHub

---
+++ 3sum/3sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i-1] != nums[i]:
+                self.twosumII(nums, i, res)
+        return res
+    
+    def twosumII(self, nums: List[int], i: int, res: List[list[int]]):
+        lo = i + 1
+        hi = len(nums) - 1
+        while (lo < hi):
+            sum = nums[i] + nums[lo] + nums[hi]
+            if sum < 0:
+                lo += 1
+            elif sum > 0:
+                hi -= 1
+            else:
+                res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+                while lo < hi and nums[lo] == nums[lo - 1]:
+                    lo += 1
+            
