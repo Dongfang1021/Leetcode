35b3dce5768b49515ba0a6def6c55f7c439397ae
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-09-19 21:00:54 -0700

    Time: 325 ms (17.27%), Space: 15.3 MB (40.98%) - LeetHub

---
+++ verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        stack = []
+        lower = -1 << 31
+        for x in preorder:
+            if x < lower:
+                return False
+            while stack and x > stack[-1]:
+                lower = stack.pop()
+            stack.append(x)
+        return True
