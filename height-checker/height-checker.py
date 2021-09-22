07f045b60b5fdc5d24f85f3beee8894fa8bd59b4
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-09-22 16:21:52 -0700

    Time: 28 ms (96.64%), Space: 14.3 MB (11.28%) - LeetHub

---
+++ height-checker/height-checker.py
@@ -0,0 +1,7 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        count  = 0
+        for n, m in zip(heights, sorted(heights)):
+            if n != m:
+                count += 1
+        return count
