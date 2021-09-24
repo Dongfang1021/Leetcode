095bbd9a673df515a264548dfea4b56ce641da90
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-09-24 15:33:01 -0700

    Time: 232 ms (57.03%), Space: 19.7 MB (30.48%) - LeetHub

---
+++ design-hashset/design-hashset.py
@@ -0,0 +1,67 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+    
+    def _hash(self, key):
+        return key % self.keyRange
+    
+    def add(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
+        
+    def remove(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
+        
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+class Bucket:
+    def __init__(self):
+        self.head = Node(0)
+        
+    def insert(self, newValue):
+        if not self.exists(newValue):
+            newNode = Node(newValue, self.head.next)
+            self.head.next = newNode
+    
+    def delete(self, value):
+        prev = self.head
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+    
+    def exists(self, value):
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                return True
+            curr = curr.next
+        return False
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
