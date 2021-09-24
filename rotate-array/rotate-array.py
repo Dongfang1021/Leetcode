a5be4eeeea0a51526ba4bbc68f0f7b42710ad846
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-09-24 09:18:59 -0700

    Time: 220 ms (74.51%), Space: 25.6 MB (53.73%) - LeetHub

---
+++ rotate-array/rotate-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        a = [0]*n
+        for i in range(n):
+            a[(i + k) % n] = nums[i]
+        nums[:] = a
