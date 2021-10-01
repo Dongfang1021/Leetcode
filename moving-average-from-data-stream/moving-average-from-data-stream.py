823a00e6075a55ee42066688101ec49750815b4a
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-10-01 07:08:23 -0700

    Time: 68 ms (68.48%), Space: 17.4 MB (16.65%) - LeetHub

---
+++ moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -0,0 +1,26 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size = size
+        self.queue = [0] * self.size
+        self.head = self.window_sum = 0
+        # number of elements seen so far
+        self.count = 0
+
+    def next(self, val: int) -> float:
+        self.count += 1
+        # calculate the new sum by shifting the window
+        tail = (self.head + 1) % self.size
+        self.window_sum = self.window_sum - self.queue[tail] + val
+        # move on to the next head
+        self.head = (self.head + 1) % self.size
+        self.queue[self.head] = val
+        return self.window_sum / min(self.size, self.count)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
