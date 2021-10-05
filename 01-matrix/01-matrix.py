a34d13c017ae0e419d6852dd9b9fdf426dd08b58
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-10-04 21:23:50 -0700

    Time: 1205 ms (12.40%), Space: 17.4 MB (43.19%) - LeetHub

---
+++ 01-matrix/01-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        visited = set()
+        from collections import deque
+        q = deque()
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 0:
+                    visited.add((i,j))
+                    q.append((i,j))
+        
+        while q:
+            x,y = q.popleft()
+            for dirr in [(1,0), (-1,0), (0,1), (0,-1)]:
+                newX, newY = x+dirr[0], y+dirr[1]
+                if newX >= 0 and newX <= len(mat)-1 and newY >= 0 and newY <= len(mat[0])-1 and (newX, newY) not in visited:
+                        mat[newX][newY] = mat[x][y] + 1
+                        visited.add((newX, newY))
+                        q.append((newX, newY))
+        return mat
