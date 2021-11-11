3e2267600d0745a3b4fe9cf6d1c22873f1128014
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-11-11 12:31:06 -0800

    Time: 28 ms (87.62%), Space: 14.2 MB (47.50%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,16 +6,27 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root is None:
-            return []
-        self.rtn = []
-        self.helper(root)
-        return self.rtn
-    
-    def helper(self, node):
-        if node.left:
-            self.helper(node.left)
-        self.rtn.append(node.val)
-        if node.right:
-            self.helper(node.right)
+        nodes_val,track=[],[]
+        
+        while root!=None:
+            
+            if root.left!=None:
+                track.append(root)  # since Left node should come first, we keep root for next turn
+                root=root.left
+            elif root.right!=None:   # when left is None but right node exists
+                nodes_val.append(root.val)  # we push the root value and traverse the right sub tree
+                root=root.right
+            else:
+                nodes_val.append(root.val)   # when we have reached a leaf node, we push the value and pop from stack
+                if len(track)>0:
+                    root=track.pop()
+                    if root!=None:
+                        root.left=None  #  eliminating the left sub tree, since it has already been traversed
+                else:
+                    root=None  # when track is empty
+                    
+        return nodes_val
+                
+
+
         
