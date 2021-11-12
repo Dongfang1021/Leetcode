b2bdfce6e38717ba7787b53d9dff60866d79af15
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-11-12 08:45:10 -0800

    Time: 36 ms (67.76%), Space: 15.1 MB (14.91%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -9,7 +9,7 @@ class Solution:
         levels = []
         if not root:
             return levels
-        def helper(node,level):
+        def helper(node, level):
             if len(levels) == level:
                 levels.append([])
             levels[level].append(node.val)
@@ -17,7 +17,7 @@ class Solution:
                 helper(node.left, level + 1)
             if node.right:
                 helper(node.right, level + 1)
-                
         helper(root, 0)
         return levels
+
         
