2a9928ecb5248459e6290605dfd5f46e3546021f
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-11-18 08:59:03 -0800

    Time: 44 ms (52.87%), Space: 14.7 MB (24.01%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,40 +1,41 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def find_rotate_index(left, right):
+        
+        def find_rotate_index(left,right):
             if nums[left] < nums[right]:
                 return 0
             while left <= right:
                 pivot = (left + right) // 2
-                if nums[pivot] > nums[pivot + 1]: # nums[pivot] is the largest number in nums
+                if nums[pivot] > nums[pivot + 1]:
                     return pivot + 1
                 else:
-                    if nums[pivot] < nums[left]: # nums[pivot] after rotate index
+                    if nums[pivot] < nums[left]:
                         right = pivot - 1
-                    else: # nums[pivot] before rotate index
+                    else:
                         left = pivot + 1
-        
-        def search(left, right): # binary search function
+        def bisearch(left, right):
             while left <= right:
                 pivot = (left + right) // 2
                 if nums[pivot] == target:
                     return pivot
                 else:
-                    if target < nums[pivot]:
+                    if nums[pivot] > target:
                         right = pivot - 1
                     else:
                         left = pivot + 1
             return -1
+                    
         
-        n = len(nums)
-        if n == 1: # array just has one integer
+        n = len (nums)
+        if n == 1:
             return 0 if nums[0] == target else -1
         rotate_index = find_rotate_index(0, n-1)
         if nums[rotate_index] == target:
             return rotate_index
         if rotate_index == 0:
-            return search(0, n-1)
+            return bisearch(0, n-1)
         if target < nums[0]:
-            return search(rotate_index, n - 1)
-        return search(0, rotate_index)
+            return bisearch(rotate_index, n - 1)
+        return bisearch(0, rotate_index)
     
         
