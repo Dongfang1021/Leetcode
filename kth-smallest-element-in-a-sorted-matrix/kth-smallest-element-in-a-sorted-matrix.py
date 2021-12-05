d0371e4c181d05230bf235abf7758f3eef9538e6
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-04 16:21:40 -0800

    Time: 204 ms (59.58%), Space: 18.6 MB (90.08%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,29 +1,16 @@
 class Solution:
-    
-    def countLessEqual(self, matrix, mid, smaller, larger):
-        count, n = 0, len(matrix)
-        row, col = n-1, 0
-        while row >= 0 and col < n:
-            if matrix[row][col] > mid:
-                larger = min(larger, matrix[row][col])
-                row -= 1
-            else:
-                smaller = max(smaller, matrix[row][col])
-                count += row + 1
-                col += 1
-        return count, smaller, larger
-    
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
-        start, end = matrix[0][0], matrix[n-1][n-1]
-        while start < end:
-            mid = start + (end - start) / 2
-            smaller, larger = (matrix[0][0], matrix[n-1][n-1])
-            count, smaller, larger = self.countLessEqual(matrix, mid, smaller, larger)
-            if count == k:
-                return smaller
-            if count < k:
-                start = larger
-            else:
-                end = smaller
-        return start
+        N = len(matrix)
+        minHeap = []
+        for r in range(min(k, N)):
+            minHeap.append((matrix[r][0], r, 0))
+        
+        heapq.heapify(minHeap)
+        while k:
+            element, r, c = heapq.heappop(minHeap)
+            if c < N - 1:
+                heapq.heappush(minHeap, (matrix[r][c+1], r, c+1))
+            k -= 1
+            
+        return element
+        
