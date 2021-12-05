b4c5edc85c2dcc3fdc8ffd0f3523c762e1a615ad
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-04 16:09:07 -0800

    Time: 100 ms (96.45%), Space: 14.7 MB (18.93%) - LeetHub

--- the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
+++ the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
@@ -1,11 +1,30 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        store = []
-        for i, val in enumerate(mat):
-            store.append([i, sum(val)])
-        store.sort(key = lambda x: x[1])
-        return [store[i][0] for i in range(k)]
-        
-    
-
-    
+        m = len(mat)
+        n = len(mat[0])
+        def binary_search(row):
+            low = 0
+            high = n
+            while low < high:
+                mid = low + (high - low) // 2
+                if row[mid] == 1:
+                    low = mid + 1
+                else:
+                    high = mid
+            return low
+        pq= []
+        for i, row in enumerate(mat):
+            strength = binary_search(row)
+            entry = (-strength, -i)
+            if len(pq) < k or entry > pq[0]:
+                heapq.heappush(pq, entry)
+            if len(pq) > k:
+                heapq.heappop(pq)
+                
+        indexes = []
+        while pq:
+            strength, i = heapq.heappop(pq)
+            indexes.append(-i)
+            
+        indexes = indexes[::-1]
+        return indexes
