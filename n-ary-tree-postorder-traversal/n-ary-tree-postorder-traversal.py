8874023fb784638ada83fb106aebb228cbc9ab3f
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-09 11:15:06 -0800

    Time: 48 ms (83.61%), Space: 16.2 MB (27.37%) - LeetHub

---
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            if root is not None:
+                output.append(root.val)
+            for c in root.children:
+                stack.append(c)
+        return output[::-1]
+        
