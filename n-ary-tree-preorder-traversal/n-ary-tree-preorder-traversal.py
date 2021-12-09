0682dd6e22371436e938b961185af34aa0b60291
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-09 11:08:27 -0800

    Time: 40 ms (98.95%), Space: 16 MB (65.93%) - LeetHub

---
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack, output = [root, ], []
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            stack.extend(root.children[::-1])
+        return output
+        
