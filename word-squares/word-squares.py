6520fd2d96156d1324007ef74d4ea564eacc39b7
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-11 17:41:45 -0800

    Time: 512 ms (39.18%), Space: 16.2 MB (66.63%) - LeetHub

---
+++ word-squares/word-squares.py
@@ -0,0 +1,46 @@
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        self.words = words
+        self.N = len(words[0])
+        self.buildTrie(self.words)
+
+        results = []
+        word_squares = []
+        for word in words:
+            word_squares = [word]
+            self.backtracking(1, word_squares, results)
+        return results
+
+    def buildTrie(self, words):
+        self.trie = {}
+
+        for wordIndex, word in enumerate(words):
+            node = self.trie
+            for char in word:
+                if char in node:
+                    node = node[char]
+                else:
+                    newNode = {}
+                    newNode['#'] = []
+                    node[char] = newNode
+                    node = newNode
+                node['#'].append(wordIndex)
+
+    def backtracking(self, step, word_squares, results):
+        if step == self.N:
+            results.append(word_squares[:])
+            return
+
+        prefix = ''.join([word[step] for word in word_squares])
+        for candidate in self.getWordsWithPrefix(prefix):
+            word_squares.append(candidate)
+            self.backtracking(step+1, word_squares, results)
+            word_squares.pop()
+
+    def getWordsWithPrefix(self, prefix):
+        node = self.trie
+        for char in prefix:
+            if char not in node:
+                return []
+            node = node[char]
+        return [self.words[wordIndex] for wordIndex in node['#']]
