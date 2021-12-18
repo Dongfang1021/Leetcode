c9f8686967d0cdacc12f7f491f3eee64863860b8
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-18 15:50:22 -0800

    Time: 160 ms (93.13%), Space: 15.3 MB (69.37%) - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -6,6 +6,6 @@ class Solution:
         pos = 0
         for i in range(len(nums)):
             el = nums[i]
-            if el != 0:
+            if el!= 0:
                 nums[pos], nums[i] = nums[i], nums[pos]
                 pos += 1
