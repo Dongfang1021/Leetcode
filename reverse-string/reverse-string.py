949309f93eaf146c6f3c354a48c4fd58b8de8891
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-19 16:18:22 -0800

    Time: 196 ms (81.07%), Space: 18.8 MB (18.73%) - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -6,4 +6,4 @@ class Solution:
         left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
-            left, right = left +1, right - 1
+            left, right = left + 1, right - 1
