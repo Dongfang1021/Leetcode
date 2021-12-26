da149192493704dfad06e8a3b8e2e2e7d689a11c
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2021-12-26 14:32:06 -0800

    Time: 96 ms (71.66%), Space: 14.4 MB (42.51%) - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,7 +1,6 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         N = 9
-        # Use hash set to record the status
         rows = [set() for _ in range(N)]
         cols = [set() for _ in range(N)]
         boxes = [set() for _ in range(N)]
@@ -16,9 +15,8 @@ class Solution:
                 if val in cols[c]:
                     return False
                 cols[c].add(val)
-                idx = (r//3) * 3 + c//3
+                idx = (r//3)*3+c//3
                 if val in boxes[idx]:
                     return False
                 boxes[idx].add(val)
         return True
-        
