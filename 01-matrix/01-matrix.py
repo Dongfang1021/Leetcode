3544dcd936578f1ec2cd86b16bee5f9b7a5e5c6e
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-08 14:13:50 -0800

    Time: 800 ms (40.62%), Space: 17.4 MB (30.29%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -6,10 +6,10 @@ class Solution:
             for j in range(len(mat[0])):
                 if mat[i][j] == 0:
                     visited.add((i, j))
-                    q.append((i,j))
+                    q.append((i, j))
         while q:
             x, y = q.popleft()
-            for dirr in [(1,0), (-1,0), (0,1), (0, -1)]:
+            for dirr in [(1,0), (-1, 0), (0, 1), (0, -1)]:
                 newX, newY = x + dirr[0], y+dirr[1]
                 if newX >= 0 and newX <= len(mat) - 1 and newY >= 0 and newY <= len(mat[0]) - 1 and (newX, newY) not in visited:
                     mat[newX][newY] = mat[x][y]+1
