d597b74ac8da6ad45ede366bfffcb81e26ab70f4
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-07 19:11:40 -0800

    Time: 145 ms (5.06%), Space: 14.7 MB (10.19%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -7,14 +7,14 @@ class Solution:
         def dfs(r, c):
             if image[r][c] == color:
                 image[r][c] = newColor
-                if r >=1:
-                    dfs(r - 1, c)
+                if r >= 1:
+                    dfs(r-1, c)
                 if r + 1 < R:
                     dfs(r+1, c)
                 if c >= 1:
-                    dfs(r, c-1)
+                    dfs(r, c - 1)
                 if c + 1 < C:
-                    dfs(r, c + 1)
+                    dfs(r, c+1)
         dfs(sr, sc)
         return image
         
