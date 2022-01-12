456edb3c52b9a564bffc06036aa583a329808a32
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-11 22:26:40 -0800

    Time: 137 ms (17.43%), Space: 14.3 MB (95.39%) - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -3,6 +3,7 @@ class Solution:
         h = len(haystack)
         n = len(needle)
         for i in range(h - n + 1):
-            if haystack[i: i+n] == needle:
+            if haystack[i:i+n] == needle:
                 return i
         return -1
+        
