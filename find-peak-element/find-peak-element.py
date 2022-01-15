371886dbf54cfa47e6ab5936c42e93d49fd5ba27
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-14 17:15:12 -0800

    Time: 46 ms (60.29%), Space: 14.4 MB (43.95%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -2,18 +2,14 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         nums.append(float("-inf"))
         nums.insert(0, float("-inf"))
-        
         return self.helper(nums, 1, len(nums))
     
     def helper(self, nums, start, finish):
-        if start > finish: return
-        
-        middle = start + (finish - start) // 2
-        
-        if nums[middle] > nums[middle - 1] and nums[middle] >  nums[middle + 1]:
+        if start > finish: return 
+        middle = (start+finish) // 2
+        if nums[middle] > nums[middle - 1] and nums[middle] > nums[middle+1]:
             return middle - 1
-        
         val = self.helper(nums, start, middle - 1)
-        if val is None: val = self.helper(nums, middle + 1, finish)
-        
+        if val is None:
+            val = self.helper(nums, middle + 1, finish)
         return val
