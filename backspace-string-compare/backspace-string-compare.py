4963396ddc15a1c24b90b6095d6d924c7de68c10
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-16 20:08:18 -0800

    Time: 28 ms (88.83%), Space: 14.4 MB (19.33%) - LeetHub

---
+++ backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,13 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == "#":
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+        return all(x == y for x, y in itertools.zip_longest(F(s), F(t)))
+        
