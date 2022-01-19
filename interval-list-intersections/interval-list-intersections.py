7066296bf3296ecf60a846fa1ef7b6fe67a474ab
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-19 10:18:30 -0800

    Time: 203 ms (37.09%), Space: 14.9 MB (92.86%) - LeetHub

--- interval-list-intersections/interval-list-intersections.py
+++ interval-list-intersections/interval-list-intersections.py
@@ -2,18 +2,17 @@ class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         intersection = []
         i, j = 0, 0
-        # Traverse
         while i < len(firstList) and j < len(secondList):
-            # Overlap
             if secondList[j][1] >= firstList[i][0]:
                 start = max(firstList[i][0], secondList[j][0])
                 end = min(firstList[i][1], secondList[j][1])
-                # Otherwise implies no overlap
                 if start <= end:
                     intersection.append([start, end])
-            # Update pointers
             if firstList[i][1] < secondList[j][1]:
                 i += 1
             else:
                 j += 1
         return intersection
+        
+    
+        
