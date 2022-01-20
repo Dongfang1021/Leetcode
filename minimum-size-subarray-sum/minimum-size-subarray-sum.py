1e35c3b6bb8dc07bb3b1493a8de1e032753ac90f
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-19 17:01:20 -0800

    Time: 139 ms (14.88%), Space: 16.7 MB (7.31%) - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -3,14 +3,14 @@ class Solution:
         p1 = 0
         p2 = 0
         total = 0
-        res = float('inf')
-        while (p2<len(nums)):
+        res = float("inf")
+        while (p2 < len(nums)):
             total = total + nums[p2]
             if total < target:
                 p2 += 1
             else:
-                res = min(res, p2-p1+1)
+                res = min(res, p2 - p1 + 1)
                 total = total - nums[p1] - nums[p2]
                 p1 += 1
-        return res if res<float('inf') else 0
+        return res if res<float("inf") else 0 
         
