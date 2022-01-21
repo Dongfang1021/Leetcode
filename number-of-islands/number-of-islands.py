7ce480f377fe48c22da558226e1396cbd7c2fa4e
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-21 13:57:26 -0800

    Time: 260 ms (99.07%), Space: 16.6 MB (84.16%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -6,7 +6,7 @@ class Solution:
         islands = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':  # run bfs
+                if grid[i][j] == '1':
                     islands += 1
                     dq.append([i, j])
                     grid[i][j] = '-1'
@@ -14,21 +14,18 @@ class Solution:
                         x, y = dq.popleft()
                         if x - 1 >= 0:
                             if grid[x - 1][y] == '1':
-                                grid[x - 1][y] = '-1'
-                                dq.append([x - 1, y])
-
+                                grid[x-1][y] = '-1'
+                                dq.append([x-1, y])
                         if y - 1 >= 0:
-                            if grid[x][y - 1] == '1':
-                                grid[x][y - 1] = '-1'
+                            if grid[x][y-1] == '1':
+                                grid[x][y-1] = '-1'
                                 dq.append([x, y - 1])
-
                         if x + 1 < m:
-                            if grid[x + 1][y] == '1':
-                                grid[x + 1][y] = '-1'
-                                dq.append([x + 1, y])
-
+                            if grid[x+1][y] == '1':
+                                grid[x+1][y] = '-1'
+                                dq.append([x+1, y])
                         if y + 1 < n:
-                            if grid[x][y + 1] == '1':
-                                grid[x][y + 1] = '-1'
-                                dq.append([x, y + 1])
+                            if grid[x][y+1] == '1':
+                                grid[x][y+1] = '-1'
+                                dq.append([x, y+1])
         return islands
