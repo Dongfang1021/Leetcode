c5c8f7f0fad1f703b5ea5e2f36e43231ec83281c
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-01-28 17:44:51 -0800

    Time: 32 ms (75.05%), Space: 13.8 MB (99.93%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -3,7 +3,7 @@ class Solution:
         if len(digits) == 0:
             return []
         letters = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl",
-                  "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+                  "6":"mno", "7":"pqrs", '8':'tuv', "9":"wxyz"}
         def backtrack(index, path):
             if len(path) == len(digits):
                 combinations.append("".join(path))
@@ -16,4 +16,5 @@ class Solution:
         combinations = []
         backtrack(0, [])
         return combinations
+                
         
