997fd20577c89e4d6aa6eace402e8598e9273e8d
Dongfang1021 <83881912+Dongfang1021@users.noreply.github.com>
2022-04-10 13:27:38 -0700

    Time: 65 ms (9.61%), Space: 13.8 MB (70.40%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,11 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        maxRobbedAmount = [None for _ in range(len(nums)+1)]
-        N = len(nums)
-        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
-        for i in range(N-2, -1, -1):
-            maxRobbedAmount[i] = max(maxRobbedAmount[i+1], maxRobbedAmount[i+2]+ nums[i])
-        return maxRobbedAmount[0]
+        yes = 0
+        no = 0
+        for i in nums:
+            no, yes = max(no, yes), i + no
+        return max(no, yes)
         
